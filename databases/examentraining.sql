ef7b3a6aea56c772f780aa8b4ced1e6e28e3e502
babajesse <154566@csgaugustinus.nl>
2023-04-19 10:52:07 +0000

    SDFS

--- databases/examentraining.sql
+++ databases/examentraining.sql
@@ -1,1 +1,12 @@
 DROP DATABASE IF EXISTS `examentraining`;
+
+CREATE DATABASE 'examentraining';
+
+USE `examentraining`;
+
+CREATE TABLE `vakken` (
+`vak` varchar(16) PRIMARY KEY,
+`docentcode` varchar(3) NOT NULL,
+`docentnaam` text NOT NULL,
+`lokaal` tekst
+);
